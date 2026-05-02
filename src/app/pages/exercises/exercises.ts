44f1c9537c71ce26f868a62e93e7702f36f20982
Nick H <nmh1992@hotmail.com>
2026-05-02 14:21:21 -0400

    feat: add initial skeleton for the pages of the app

---
+++ src/app/pages/exercises/exercises.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercises',
+  imports: [],
+  templateUrl: './exercises.html',
+  styleUrl: './exercises.css',
+})
+export class Exercises {}
---
+++ src/app/pages/workout/workout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workout',
+  imports: [],
+  templateUrl: './workout.html',
+  styleUrl: './workout.css',
+})
+export class Workout {}
